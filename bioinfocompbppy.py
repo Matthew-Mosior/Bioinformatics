97e6e00199b63e06ec9f8b3f7aa86f85cf1360bd
Matthew Mosior <Matthew.Mosior@gmail.com>
2017-10-30 00:21:23 -0500

    Add files via upload

---
+++ bioinfocompbppy.py
@@ -0,0 +1,83 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "Matt's 2 in 1"
+__date__ = "$Oct 28, 2017 12:49:40 AM$"
+
+#This program will take a users DNA sequence input and return the complementary DNA sequence.
+#Program accomplished task without use of Bio.seq imports (Biopython).
+
+#imports:
+
+import re
+import os
+import sys
+
+#Functions:
+
+def getuserinput():
+    #Function to gather user DNA sequence.
+    print ("Welcome to the getuserinput function!")
+    print ("This function will take user DNA sequence input and return it.")
+    user_DNA_input = input('Please enter your DNA sequence: ')
+    #Ensure user input is only a upper or lower case nucleotide.
+    if not re.match("^[atgcATGC]", user_DNA_input):
+        print ("Sorry, that is not a valid DNA sequence.")
+        sys.exit()
+    #Print user DNA sequence string.
+    print("Here is the DNA sequence you entered:")
+    print(user_DNA_input)
+    #Return user DNA sequence.
+    return user_DNA_input
+
+def touppercase(userDNAinput):
+    #Function to make any lowercase nucleotides uppercase.
+    #Print uppercase user DNA sequence string.
+    print("Here is the upper case user DNA sequence:")
+    print (userDNAinput.upper())
+    #Return the uppercase DNA sequence.
+    return userDNAinput.upper()
+
+def reverse(userDNAinput):
+    #Function to reverse the user DNA sequence input.
+    #Split the user DNA sequence at each nucleotide and store as a list.
+    userDNAinput.split()
+    #Reverse the user DNA sequence list.
+    reverseDNA = userDNAinput[::-1]
+    #Print user DNA sequence list.
+    print ("Here is the reversed user DNA sequence.")
+    print(reverseDNA)
+    #Return the reversed DNA sequence.
+    return reverseDNA
+
+def complement(userDNAinput):
+    #Function to provide complementary DNA sequence to original DNA sequence input.
+    #Create dictionary of DNA nucleotides.
+    new_DNA_dictionary = {'A':'T', 'T':'A', 'G':'C', 'C':'G'}
+    #Use dictionary to create complementary DNA sequence.
+    print("Here is the complementary user DNA sequence:")
+    print (''.join([new_DNA_dictionary[usernucleotide] for usernucleotide in userDNAinput]))
+    
+def main():
+    print ("Welcome to Matt's DNA sequence reader.")
+    
+    #Gather user DNA sequence input.
+    UserDNAseq = getuserinput()
+    
+    #Check uppercase or lower case nucleotides.
+    upperDNAseq = touppercase(UserDNAseq)
+    
+    #Reverse list of DNA nucleotides.
+    reverseDNAseq = reverse(upperDNAseq)
+    
+    #Compute complementary DNA sequence.
+    complement(reverseDNAseq)
+    
+    print ("Thanks for using Matt's DNA sequence reader!")
+    
+    #Exit the program.
+    sys.exit()
+    
+if __name__ == "__main__":
+    main()
