1fa9fcb00eff1ef0da3125499266f24a7a1ba8ef
Matthew Mosior <Matthew.Mosior@gmail.com>
2017-11-15 17:26:44 -0600

    Add files via upload

---
+++ rosalindcountnucleo.hs
@@ -0,0 +1,35 @@
+--Rosalind: Counting DNA Nucleotides.
+
+module Main where
+
+
+
+--Imports
+
+import Data.List
+
+
+
+--Type declarations with corresponding function definitions.
+
+
+--count
+
+count :: Char -> String -> Int
+
+count x xs = length[x' | x' <- xs , x == x']
+
+
+
+--nucleotidecounter
+nucleotidecounter :: String -> [Int]
+
+nucleotidecounter xs = [count x xs | x <- ['A','C','G','T']] 
+
+
+
+
+--IO function.
+
+main :: IO ()
+main = putStrLn ("This Haskell script will return the count of each nucleotide, in the order of A,C,G,T, in a given string.")
