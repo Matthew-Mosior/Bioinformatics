45f15ff50b317b848855760fdd16d83c41587485
Matthew Mosior <Matthew.Mosior@gmail.com>
2017-11-15 17:20:33 -0600

    Add files via upload

--- Main.hs
+++ Main.hs
@@ -1,20 +1,29 @@
---Rosalind: Counting DNA Nucleotides.
+--Reverse complement DNA sequence Haskell script.
+
 module Main where
 
---Imports
-import Data.List
+import Data.Char 
+import Data.List   
 
---Type declarations with corresponding function definitions.
+--types and corresponding function declarations:
 
---count
-count :: Char -> String -> Int
-count x xs = length[x' | x' <- xs , x == x']
+--main IO function.
+main :: IO()
 
---nucleotidecounter
-nucleotidecounter :: String -> [Int]
-nucleotidecounter xs = [count x xs | x <- ['A','C','G','T']] 
+--User DNA sequence input to capitalized reversed string.
+userInputreverse :: String -> String
+userInputreverse   [] = []
+userInputreverse (x:xs) = if x == 'a' || x == 't' || x == 'g' || x == 'c' || x == 'A' || x == 'T' || x == 'G' || x == 'C' then reverse(map toUpper(x:rest)) else rest
+                   where rest = reverse (userInputreverse xs)
+
+--Another way to write DNA sequence capitalized reversed string function (with addition of finding complement DNA):
+userInputcomplement :: String -> String
+userInputcomplement x = let 
+                        repl 'A' = 'T' 
+                        repl 'T' = 'A' 
+                        repl 'G' = 'C' 
+                        repl 'C' = 'G' 
+                    in map repl (reverse ( map toUpper (filter (`elem` "atgcATGC")x)))
 
 
---IO function.
-main :: IO ()
-main = putStrLn ("This Haskell script will return the count of each nucleotide in a given string.")
+main = putStrLn ("These functions are used to find the reverse and reverse complement to a user DNA sequence input.")
